009da0c77b5d3138cfc64d5800cf7536dc2d0b8e
Sam Acer <sam@sam.fr>
2023-07-31 15:21:14 +0200

    fix: ex02/module9 finish

--- module_9/ex02/PmergeMe.hpp
+++ module_9/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: snaggara <snaggara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 14:22:51 by snaggara          #+#    #+#             */
-/*   Updated: 2023/07/31 13:45:12 by snaggara         ###   ########.fr       */
+/*   Updated: 2023/07/31 15:00:45 by snaggara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,14 +56,14 @@ typedef intVector_t::iterator intVectorIt_t;
 typedef std::deque<std::pair<int, int> > pairDeque_t;
 typedef pairDeque_t::iterator pairDequeIt_t;
 
-//template<typename T>
+template<typename T>
 class PmergeMe
 {
 
 	pairDeque_t 	_pairDeque;
-	intDeque_t 		_originalDeque;
-	intDeque_t 		_firstPairDeque;
-	intDeque_t 		_secondPairDeque;
+	T 				_originalDeque;
+	T 				_firstPairDeque;
+	T 				_secondPairDeque;
 	int				_maxI;
 	intDeque_t		_jacobsthalSuit;
 	struct timeval	_start;
@@ -73,31 +73,32 @@ class PmergeMe
 
 	PmergeMe(void);
 	PmergeMe(PmergeMe const& other);
-	PmergeMe&		operator=(PmergeMe const& other);
+	PmergeMe<T>&		operator=(PmergeMe const& other);
 
-	intDeque_t	 	_sortOneElement(intDeque_t& originalDeque);
+	T	 			_sortOneElement(T& originalDeque);
 	void			_createPairs();
 	static void 	_orderPairDecreasing(std::pair<int,int> &pair);
 	static int		_get_first(std::pair<int, int> pair);
 	void			_addSecondElementsWithJacob();
-	void			_add_by_dichotomie(int nb, intdequeIt_t begin, int length);
+	void			_add_by_dichotomie(int nb, typename T::iterator begin, int length);
 	void			_sortEachPair();
 	void			_createFirstPairDeque();
 	void			_createSecondPairDeque();
 	void			_addLastIfOdd();
 
 
-	void			_createJacobsthalDeque(unsigned long nmoins1, unsigned long nmoins2);
+	void			_createJacobsthal(unsigned long nmoins1, unsigned long nmoins2);
 	void			_nextNumberJacob(int & nb);
 	int				_jacobOffset(int const &nb);
 
 public:
 	PmergeMe(intDeque_t& originalDeque);
+	PmergeMe(intVector_t& originalDeque);
 	~PmergeMe(){};
 
-	intDeque_t	mergeSort();
+	T			mergeSort();
 	void		buildJacNumberSuit();
-	void		_advanceCursor(int &i, intdequeIt_t &itD2, int &length);
+	void		_advanceCursor(int &i, typename T::iterator &itD2, int &length);
 	double		getOperationTime() const;
 
 
@@ -147,14 +148,329 @@ public:
 	};
 };
 
+#include "PmergeMe.cpp"
 
+
+template <typename T>
+PmergeMe<T>::PmergeMe(){};
+
+/*A la construction, on initialise statiquement le jacobien, si il n'existe pas*/
+template <typename T>
+PmergeMe<T>::PmergeMe(intDeque_t& originalDeque)
+	:_originalDeque(originalDeque)
+{
+    gettimeofday(&_start, NULL);
+	
+	if (_jacobsthalSuit.size() == 0)
+	{
+		_createJacobsthal(1, 1);
+		_jacobsthalSuit.pop_front();
+		_jacobsthalSuit.push_back(-1);
+	}
+};
+
+template <typename T>
+PmergeMe<T>&	PmergeMe<T>::operator=(PmergeMe const& other)
+{
+	static_cast<void>(other);
+	return (*this);
+}
+
+template <typename T>
+PmergeMe<T>::PmergeMe(PmergeMe const& other)
+	{(void)other;}
+
+
+template <typename T>
+T	PmergeMe<T>::mergeSort()
+{
+	if (_originalDeque.size() == 1)
+		return _sortOneElement(_originalDeque);
+
+	_createPairs();
+	_sortEachPair();
+	_createFirstPairDeque();
+	PmergeMe<T> merge(_firstPairDeque);
+	_firstPairDeque = merge.mergeSort();
+	_createSecondPairDeque();
+	_addSecondElementsWithJacob();
+	_addLastIfOdd();
+	gettimeofday(&_end, NULL);
+	return (_firstPairDeque);
+}
+
+template <typename T>
+void	PmergeMe<T>::_createSecondPairDeque()
+{
+	GetSecond getSecond(_pairDeque);
+	std::transform(_firstPairDeque.begin(), _firstPairDeque.end(), std::back_inserter(_secondPairDeque), getSecond);
+}
+
+template <typename T>
+void	PmergeMe<T>::_createFirstPairDeque()
+{
+	std::transform(_pairDeque.begin(), _pairDeque.end(), std::back_inserter(_firstPairDeque), _get_first);
+}
+
+template <typename T>
+void	PmergeMe<T>::_sortEachPair()
+{
+	std::for_each(_pairDeque.begin(), _pairDeque.end(), _orderPairDecreasing);
+}
+
+template <typename T>
+void	PmergeMe<T>::_addSecondElementsWithJacob()
+{
+	typename T::iterator itD2 = _secondPairDeque.begin();
+
+	_firstPairDeque.push_front(*itD2);
+	if (_secondPairDeque.size() == 1)
+		return ;
+	itD2++;
+	_add_by_dichotomie(*itD2, _firstPairDeque.begin(), 1);
+	itD2--;
+	if (_secondPairDeque.size() == 2)
+		return ;
+
+	int i = 0;
+	size_t compteur = 2;
+	int length = 2;
+	_maxI = 1;
+
+	while (compteur < _secondPairDeque.size())
+	{
+		compteur++;
+		_advanceCursor(i, itD2, length);
+		_add_by_dichotomie(*itD2, _firstPairDeque.begin(), length);								
+	}
+}
+
+template <typename T>
+void	PmergeMe<T>::_advanceCursor(int &i, typename T::iterator &itD2, int &length)
+{
+		int offset = _jacobOffset(i);
+		int nbMissed = 0;
+		
+		if (offset == -1)
+		{
+			itD2--;
+			i--;
+			return ;
+		}
+		
+		for (int j = 0; j < offset; j++)
+		{
+			itD2++;
+			i++;
+			if (i > _maxI)
+				nbMissed++;
+			if (itD2 == _secondPairDeque.end())
+			{
+				i--;
+				itD2--;
+				nbMissed--;
+				break ;
+			}
+		}
+		length = 2 * i - nbMissed;
+		nbMissed = 0;
+		_maxI = i;
+}
+
+template <typename T>
+void	PmergeMe<T>::_addLastIfOdd()
+{
+	if (_originalDeque.size() % 2 == 1)
+	{
+		_add_by_dichotomie(*(_originalDeque.end() - 1), _firstPairDeque.begin(), _firstPairDeque.size() - 1);
+	}
+}
+
+/*
+	Fonction qui marche par recursion.
+	Si la longueur est 1, on insere soit a gauche soit a droite
+	Si c'est au dessus, on compare avec ce qui est au milieu, et on rappel
+	reccursivement soit a gauche soit a droite 
+*/
+template <typename T>
+void	PmergeMe<T>::_add_by_dichotomie(int nb, typename T::iterator begin, int length)
+{
+	int	half = (length / 2);
+	
+	if (length == 0)
+	{
+		if (nb < *begin)
+			_firstPairDeque.insert(begin, nb);
+		else if (nb > *begin)
+			_firstPairDeque.insert(begin + 1, nb);
+		else
+			throw DuplicateNumbers(nb);
+	}
+	else if (length == 1)
+	{
+		if (nb < *(begin + half))
+			_add_by_dichotomie(nb, begin, half);
+		else if (nb > *(begin + half))
+			_add_by_dichotomie(nb, begin + 1, half);
+		else
+			throw DuplicateNumbers(nb);
+	}
+	else if (length % 2 == 1)
+	{
+		if (nb < *(begin + half))
+			_add_by_dichotomie(nb, begin, half - 1);
+		else if (nb > *(begin + half))
+			_add_by_dichotomie(nb, begin + half + 1, half);
+		else
+			throw DuplicateNumbers(nb);
+	}
+	else if (length % 2 == 0)
+	{
+		if (nb < *(begin + half))
+			_add_by_dichotomie(nb, begin, half - 1);
+		else if (nb > *(begin + half))
+			_add_by_dichotomie(nb, begin + half + 1, half - 1);
+		else
+			throw DuplicateNumbers(nb);
+	}
+}
+
+template <typename T>
+int	PmergeMe<T>::_get_first(std::pair<int, int> pair)
+{
+	return (pair.first);
+}
+
+template <typename T>
+void PmergeMe<T>::_orderPairDecreasing(std::pair<int,int> &pair)
+{
+	if (pair.first < pair.second)
+		std::swap(pair.first, pair.second);
+}
+
+template <typename T>
+void PmergeMe<T>::_createPairs()
+{
+	for (typename T::iterator it = _originalDeque.begin(); it != _originalDeque.end(); it+=2)
+	{
+		if (it + 1 == _originalDeque.end())
+			break ;
+		_pairDeque.push_back(std::make_pair(*it, *(it + 1)));
+	}
+}
+
+template <typename T>
+T PmergeMe<T>::_sortOneElement(T& originalDeque)
+{
+	T returndeque(1, *originalDeque.begin());
+	return (returndeque);
+}
+
+/*Cree la suite de Jacobsthal jusqu'au int max a la fin.*/
+template <typename T>
+void	PmergeMe<T>::_createJacobsthal(unsigned long nmoins2, unsigned long nmoins1)
+{
+	if (nmoins1 == 1 && nmoins2 == 1)
+	{
+		PmergeMe<T>::_jacobsthalSuit.push_back(1);
+		PmergeMe<T>::_jacobsthalSuit.push_back(1);
+	}
+	unsigned long n = 2 * nmoins2 + nmoins1;
+	if (n >= static_cast<unsigned long>(std::numeric_limits<int>::max()))
+	{
+		_jacobsthalSuit.push_back(std::numeric_limits<int>::max());
+		return ;
+	}
+
+	_jacobsthalSuit.push_back(n);
+	
+	_createJacobsthal(nmoins1, n);
+}
+
+template <typename T>
+void	PmergeMe<T>::_nextNumberJacob(int & nb)
+{
+	nb--;
+	/*Si on a un nombre qui existe dans jac
+	Alors on renvoie celui de l'index d'apres*/
+	 std::deque<int>::iterator found = std::find(_jacobsthalSuit.begin(), _jacobsthalSuit.end(), nb);
+	if (found != _jacobsthalSuit.end())
+	{
+		found+=2;
+		nb = *found;
+	}
+	else if (nb == -1)
+		nb = 3;
+}
+
+template <typename T>
+int	PmergeMe<T>::_jacobOffset(int const &nb)
+{
+	if (nb == 0)
+		return (3);
+	std::deque<int>::iterator found = std::find(_jacobsthalSuit.begin(), _jacobsthalSuit.end(), nb - 1);
+
+	if (found != _jacobsthalSuit.end())
+	{
+		found+=2;
+		return (*found - nb);
+	}
+	else
+		return (-1);
+}
+
+template <typename T>
+double	PmergeMe<T>::getOperationTime() const
+{
+	long diffSeconds = _end.tv_sec - _start.tv_sec;
+    long diffMicroseconds = _end.tv_usec - _start.tv_usec;
+	double diff = diffSeconds + diffMicroseconds * 1e-6;
+	return (diff);
+}
+
+
+/* Adaptation pour le vector */
+
+/*A la construction, on initialise statiquement le jacobien, si il n'existe pas*/
+template <typename T>
+PmergeMe<T>::PmergeMe(intVector_t& originalDeque)
+	:_originalDeque(originalDeque)
+{
+    gettimeofday(&_start, NULL);
+	
+	if (_jacobsthalSuit.size() == 0)
+	{
+		_createJacobsthal(1, 1);
+		_jacobsthalSuit.pop_front();
+		_jacobsthalSuit.push_back(-1);
+	}
+};
+
+template <>
+void	PmergeMe<intVector_t>::_addSecondElementsWithJacob()
+{
+	typename intVector_t::iterator itD2 = _secondPairDeque.begin();
+
+	_firstPairDeque.insert(_firstPairDeque.begin(), *itD2);
+	if (_secondPairDeque.size() == 1)
+		return ;
+	itD2++;
+	_add_by_dichotomie(*itD2, _firstPairDeque.begin(), 1);
+	itD2--;
+	if (_secondPairDeque.size() == 2)
+		return ;
+
+	int i = 0;
+	size_t compteur = 2;
+	int length = 2;
+	_maxI = 1;
+
+	while (compteur < _secondPairDeque.size())
+	{
+		compteur++;
+		_advanceCursor(i, itD2, length);
+		_add_by_dichotomie(*itD2, _firstPairDeque.begin(), length);								
+	}
+}
 
 #endif
-
-//(compteur) * 2 - missed
-/*
-indice 2 -> longueur 2
-indice 4 -> longueur 6
-indice 
-
-*/
--- module_9/ex02/main.cpp
+++ module_9/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: snaggara <snaggara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 12:21:48 by snaggara          #+#    #+#             */
-/*   Updated: 2023/07/30 23:59:53 by snaggara         ###   ########.fr       */
+/*   Updated: 2023/07/31 15:19:46 by snaggara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,41 @@
 #include <string>
 #include <deque>
 
+template<typename T>
+T	createContainer(int ac, char **av)
+{
+	int number;
+	T originalDeque;
+
+	for (int i = 1; i < ac; i++)
+	{
+		std::istringstream iss(av[i]);
+		iss >> number;
+		originalDeque.push_back(number);
+	}
+	return (originalDeque);
+}
+
+template<typename T>
+void	displayContainer(T container)
+{
+	for (typename T::iterator it = container.begin(); it != container.end(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+}
+
+double	getDiffTime(struct timeval start)
+{
+	struct timeval	end;
+
+	gettimeofday(&end, NULL);
+	long diffSeconds = end.tv_sec - start.tv_sec;
+	long diffMicroseconds = end.tv_usec - start.tv_usec;
+	double diff = diffSeconds + diffMicroseconds * 1e-6;
+	return (diff);
+
+}
+
 int	main(int ac, char **av)
 {
 	if (ac < 2)
@@ -24,31 +59,51 @@ int	main(int ac, char **av)
 		return (0);
 	}
 
-	int number;
-	std::deque<int> originalDeque;
-	for (int i = 1; i < ac; i++)
-	{
-		std::istringstream iss(av[i]);
-		iss >> number;
-		originalDeque.push_back(number);
-	}
-	std::cout << "\nBefore : ";
-	for (std::deque<int>::iterator it = originalDeque.begin(); it != originalDeque.end(); it++)
-		std::cout << *it << " ";
-	std::cout << std::endl;
 
 	try{
-		PmergeMe merge(originalDeque);
-		std::deque<int> result = merge.mergeSort();
+		struct timeval	start;
+    	gettimeofday(&start, NULL);
+
+		intDeque_t originalDeque = ::createContainer<intDeque_t >(ac, av);
+
+		std::cout << "\nTest with deque : \nBefore : ";
+		displayContainer<intDeque_t>(originalDeque);
 
 		std::cout << "After :  ";
-		for (std::deque<int>::iterator it = result.begin(); it != result.end(); it++)
-			std::cout << *it << " ";
-		std::cout << std::endl << std::endl;
-		std::cout << "Time to proceed a range of " << result.size() << " elements with std::deque :"
-					<< merge.getOperationTime() * 1000 << " ms";
+		PmergeMe<intDeque_t> merge(originalDeque);
+		intDeque_t result = merge.mergeSort();
+		displayContainer<intDeque_t>(result);
+
+		double diff = ::getDiffTime(start);
+		std::cout << "\nTime to proceed a range of " << result.size() << " elements with std::deque :"
+					<< diff * 100000 << " micro s";
 	}
-	catch (PmergeMe::DuplicateNumbers const& e)
+	catch (PmergeMe<intDeque_t>::DuplicateNumbers const& e)
+	{
+		std::cout << e.what() << "\nNumber : " << e.nb << std::endl;
+	}
+
+	try{
+		struct timeval	start;
+    	gettimeofday(&start, NULL);
+
+		intVector_t originalVector = ::createContainer<intVector_t >(ac, av);
+
+		std::cout << "\n\n\nTest with vector : \nBefore : ";
+		displayContainer<intVector_t>(originalVector);
+
+		std::cout << "After :  ";
+		PmergeMe<intVector_t> merge(originalVector);
+		(void)merge;
+		intVector_t result = merge.mergeSort();
+		displayContainer<intVector_t>(result);
+
+		double diff = ::getDiffTime(start);
+		std::cout << "\nTime to proceed a range of " << result.size() << " elements with std::deque :"
+					<< diff * 1000000 << " micro s";
+
+	}
+	catch (PmergeMe<intDeque_t>::DuplicateNumbers const& e)
 	{
 		std::cout << e.what() << "\nNumber : " << e.nb << std::endl;
 	}
